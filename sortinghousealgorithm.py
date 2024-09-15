a7561fee5f97d448e8505ecfe2be5974ee5a519a
jackson-brick <jacksond.brick@gmail.com>
2024-09-15 03:23:06 +0000

    finished questions for sorting hat

---
+++ sortinghousealgorithm.py
@@ -0,0 +1,36 @@
+
+print("+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+")
+print("+                                  +")
+print("+           Sorting Hat            +")
+print("+                                  +")
+print("+             Welcome              +")
+print("+                                  +")
+print("+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+")
+
+print("The sorting house algorithm will ask you some questions to decide which Hogwarts House you belong to.")
+
+brave = input("If you were in an unfamiliar place or situation, would you feel confident in knowing what to do to get to a better place?\n")
+loyal = input("If your friend needed help but your dignity was at stake, would you help them?\n")
+chival = input("If someone else was in trouble, would you help them?\n")
+curious = input("Does the world around you and the way it works strike you as interesting?\n")
+create = input("Do you enjoy creating new things unseen or heard of before?\n")
+kind = input("Do you like helping others in need even when not asked or compelled to do so?\n")
+friend = input("Do you find yourself constantly making new friends and easily talking to strangers?\n")
+pati = input("When others do things you do not like, are you usually able to keep your cool?\n")
+ambi = input("Are you willing to do anything to achieve your goals?\n")
+comp = input("When someone is better than you at something does that upset you?\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
