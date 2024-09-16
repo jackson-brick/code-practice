1c6958740e6a50edc9c7fd659f2dcd2e2f3f691f
jackson-brick <jacksond.brick@gmail.com>
2024-09-16 03:48:37 +0000

    work done on sorting house

--- sortinghousealgorithm.py
+++ sortinghousealgorithm.py
@@ -9,8 +9,29 @@ print("+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+")
 
 print("The sorting house algorithm will ask you some questions to decide which Hogwarts House you belong to.")
 
+gryf = 1
+rave = 1
+huff = 1 
+slyt = 1
+
+
+
 brave = input("If you were in an unfamiliar place or situation, would you feel confident in knowing what to do to get to a better place?\n")
+while brave.lower() != "yes" and brave.lower != "no":
+    print("That is not a valid response. Please try again.")
+    brave = input("If you were in an unfamiliar place or situation, would you feel confident in knowing what to do to get to a better place?\n")
+if brave.lower() == "yes":
+    gryf = gryf + 1
+elif brave.lower() == "no":
+    rave = rave + 1
+    huff = huff + 1
+    slyt = slyt + 1
 loyal = input("If your friend needed help but your dignity was at stake, would you help them?\n")
+while loyal.lower() != "yes" and loyal.lower() != "no":
+    print("That is not a valid response. Please try again.")
+    loyal = input("If your friend needed help but your dignity was at stake, would you help them?\n")
+if loyal.lower() == "yes":
+    gryf =  gryf + 1
 chival = input("If someone else was in trouble, would you help them?\n")
 curious = input("Does the world around you and the way it works strike you as interesting?\n")
 create = input("Do you enjoy creating new things unseen or heard of before?\n")
