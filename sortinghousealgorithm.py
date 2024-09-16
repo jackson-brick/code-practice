e3950f3c2524e725faee53dec71aeaf1f2c58dd5
jackson-brick <jacksond.brick@gmail.com>
2024-09-16 21:26:12 +0000

    finished rough draft of sorting hat algorithm

--- sortinghousealgorithm.py
+++ sortinghousealgorithm.py
@@ -9,10 +9,10 @@ print("+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+")
 
 print("The sorting house algorithm will ask you some questions to decide which Hogwarts House you belong to.")
 
-gryf = 1
-rave = 1
-huff = 1 
-slyt = 1
+gryf = 0
+rave = 0
+huff = 0 
+slyt = 0
 
 
 
@@ -93,25 +93,25 @@ while kind.lower() != "yes" and kind.lower != "no":
 if kind.lower() == "yes":
     gryf = gryf + 1
     rave = rave - 1
-    huff = huff - 1
+    huff = huff + 1
     slyt = slyt - 1
 elif kind.lower() == "no":
     gryf = gryf - 1
     rave = rave + 1
-    huff = huff + 1
+    huff = huff - 1
     slyt = slyt + 1
 friend = input("Do you find yourself constantly making new friends and easily talking to strangers?\n")
 while friend.lower() != "yes" and friend.lower != "no":
     print("That is not a valid response. Please try again.")
     friend = input("Do you find yourself constantly making new friends and easily talking to strangers?\n")
 if friend.lower() == "yes":
-    gryf = gryf + 1
-    rave = rave - 1
+    gryf = gryf - 1
+    rave = rave + 1
     huff = huff - 1
     slyt = slyt - 1
 elif friend.lower() == "no":
-    gryf = gryf - 1
-    rave = rave + 1
+    gryf = gryf + 1
+    rave = rave - 1
     huff = huff + 1
     slyt = slyt + 1
 pati = input("When others do things you do not like, are you usually able to keep your cool?\n")
@@ -119,13 +119,13 @@ while pati.lower() != "yes" and pati.lower != "no":
     print("That is not a valid response. Please try again.")
     pati = input("When others do things you do not like, are you usually able to keep your cool?\n")
 if pati.lower() == "yes":
-    gryf = gryf + 1
-    rave = rave - 1
+    gryf = gryf - 1
+    rave = rave + 1
     huff = huff - 1
     slyt = slyt - 1
 elif pati.lower() == "no":
-    gryf = gryf - 1
-    rave = rave + 1
+    gryf = gryf + 1
+    rave = rave - 1
     huff = huff + 1
     slyt = slyt + 1
 ambi = input("Are you willing to do anything to achieve your goals?\n")
@@ -133,31 +133,38 @@ while ambi.lower() != "yes" and ambi.lower != "no":
     print("That is not a valid response. Please try again.")
     ambi = input("Are you willing to do anything to achieve your goals?\n")
 if ambi.lower() == "yes":
-    gryf = gryf + 1
+    gryf = gryf - 1
     rave = rave - 1
     huff = huff - 1
-    slyt = slyt - 1
+    slyt = slyt + 1
 elif ambi.lower() == "no":
-    gryf = gryf - 1
+    gryf = gryf + 1
     rave = rave + 1
     huff = huff + 1
-    slyt = slyt + 1
+    slyt = slyt - 1
 comp = input("When someone is better than you at something does that upset you?\n")
 while comp.lower() != "yes" and comp.lower != "no":
     print("That is not a valid response. Please try again.")
     comp = input("When someone is better than you at something does that upset you?\n")
 if comp.lower() == "yes":
-    gryf = gryf + 1
+    gryf = gryf - 1
     rave = rave - 1
     huff = huff - 1
-    slyt = slyt - 1
+    slyt = slyt + 1
 elif comp.lower() == "no":
-    gryf = gryf - 1
+    gryf = gryf + 1
     rave = rave + 1
     huff = huff + 1
-    slyt = slyt + 1
+    slyt = slyt - 1
 
-
+if gryf >= rave and gryf >= huff and gryf >= slyt:
+    print("Your Hogwarts house is Gryffindor!")
+elif rave > gryf and rave >= huff and rave >= slyt:
+    print("Your Hogwarts house is Ravenclaw!")
+elif huff > gryf and huff > rave and huff >= slyt:
+    print("Your Hogwarts house is Hufflepuff!")
+elif slyt > gryf and slyt > rave and slyt > huff:
+    print("Your Hogwarts house is Slytherin!")
 
 
 
