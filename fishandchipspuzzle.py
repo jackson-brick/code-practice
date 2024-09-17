3513aa4165547da800433553338addf4c325fcaf
jackson-brick <jacksond.brick@gmail.com>
2024-09-17 18:12:46 +0000

    did it.

---
+++ fishandchipspuzzle.py
@@ -0,0 +1,16 @@
+a = "chips"
+b = "fish"
+c = b
+b = a
+a = c
+print(a + " and " + b)
+
+
+
+total = int(input("Number."))
+
+total = total/7
+total = total * 100
+total = int(total + 0.5)
+total = total / 100
+print(total)
