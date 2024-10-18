0772f45d22051a2b6d3e6406cd2063fb91576872
jackson-brick <jacksond.brick@gmail.com>
2024-10-18 17:21:08 +0000

    fixed loading sequence

--- test.py
+++ test.py
@@ -4,13 +4,14 @@ import time
 print("this should be a loading sequence")
 
 
-def loading_sequence():
-    load = ["Loading", ".", ".", "."]
-    for loading_sequence_word in load:
-        print(loading_sequence_word, end = " ")
+def loading_sequence(seconds_to_load):
+    time.sleep(0.5)
+    print("Loading")
+    for stl in range(seconds_to_load):
+        print("   .")
         time.sleep(1)
 
 
 
 
-loading_sequence()
+loading_sequence(5)
