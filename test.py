75f8ea16976378e60c16c58b5479c2139561c248
jackson-brick <jacksond.brick@gmail.com>
2024-10-18 21:22:44 +0000

    test

--- test.py
+++ test.py
@@ -1,17 +1,8 @@
-import time
-
-
-print("this should be a loading sequence")
-
-
-def loading_sequence(seconds_to_load):
-    time.sleep(0.5)
-    print("Loading")
-    for stl in range(seconds_to_load):
-        print("   .")
-        time.sleep(1)
 
 
 
 
-loading_sequence(5)
+
+
+
+
