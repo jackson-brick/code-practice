d12ee9ad6926062069f56c319aa666fc48bcb991
jackson-brick <jacksond.brick@gmail.com>
2024-10-23 17:57:07 +0000

    test

--- test.py
+++ test.py
@@ -1,6 +1,21 @@
+import time
+import os
+
+screen_size = os.get_terminal_size()
 
 
-
+print("Loading")
+time.sleep(1)
+os.system('clear')
+print("Loading  .")
+time.sleep(1)
+os.system('clear')
+print("Loading  .  .")
+time.sleep(1)
+os.system('clear')
+print("Loading  .  .  .")
+time.sleep(1)
+os.system('clear')
 
 
 
