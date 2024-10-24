3f7722cc9b036cf85029f2cb8cf9fb459988a8c7
jackson-brick <jacksond.brick@gmail.com>
2024-10-24 18:41:59 +0000

    test

--- test.py
+++ test.py
@@ -1,21 +1,32 @@
-import time
 import os
 
-screen_size = os.get_terminal_size()
+screenSize = os.get_terminal_size()
+z = int(screenSize[0])
+
+paragraph = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair."
+mylist = []
+
+usedParagraph = paragraph.split()
+#for word in range(len(usedParagraph)):
+    #usedParagraph[word] = usedParagraph[word] + " "
 
 
-print("Loading")
-time.sleep(1)
-os.system('clear')
-print("Loading  .")
-time.sleep(1)
-os.system('clear')
-print("Loading  .  .")
-time.sleep(1)
-os.system('clear')
-print("Loading  .  .  .")
-time.sleep(1)
-os.system('clear')
+print(*usedParagraph)
+
+wordAcrossScreen = 0
+emptySpaceCounter = 0
+#for word in usedParagraph:
+    #wordAcrossScreen += int(len(word))
+    #if wordAcrossScreen > z:
+        #wordAcrossScreen -= int(len(word))
+        #for i in range(z-wordAcrossScreen):
+            #print(" ", end = "")
+        #wordAcrossScreen = 0
+    #else:
+        #print(word, end = "")
+    
+
+
 
 
 
