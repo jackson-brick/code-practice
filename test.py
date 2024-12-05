c11cc1806935690f6fb8b05e7fbf83f922be61d0
jackson-brick <jacksond.brick@gmail.com>
2024-12-05 18:15:59 +0000

    test

--- test.py
+++ test.py
@@ -5,8 +5,7 @@ correctAnswer = "c"
 mcqList.remove(correctAnswer)
 mcqList.remove(random.choice(mcqList))
 
-print(mcqList)
+randomFlashChoice = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+random.shuffle(randomFlashChoice)
 
-print(int(7.9))
-
-print("'to discover'")
+print(randomFlashChoice)
