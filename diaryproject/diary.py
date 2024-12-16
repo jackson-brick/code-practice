c6c64b3a1a7495bd88edb71031a9552a64b30c12
jackson-brick <jacksond.brick@gmail.com>
2024-12-16 20:45:55 +0000

    login/signup working

--- diaryproject/diary.py
+++ diaryproject/diary.py
@@ -2,95 +2,147 @@ import time
 import json
 import os
 from colorama import Fore, Style, Back
+import random
 
 z = os.get_terminal_size()
 z = z[0]
 
-userNum = "a"
 
 with open('diaryusers.json','r') as file:
-        diaryusers = json.load(file)
-diaryAtUser = diaryusers["user"]
+    diaryusers = json.load(file)
+diaryAtUser = diaryusers["users"]
 
-def encrypt():
-     
+with open('diaryentry.json','r') as file:
+    diaryentry = json.load(file)
+diaryAtEntry = diaryentry["entry"]
 
-def new_user():
+def encrypt(pswrd):
+    global encryptedPass
+    unencryptedPass = []
+    for ele in range(len(pswrd)):
+         unencryptedPass.append(pswrd[ele])
+    encryptedPass = []
+    for ele in unencryptedPass:
+        encryptedPass.append(diaryusers["users"][userNum]["key"][ele])
+    encryptedPass = ''.join(encryptedPass)
+
+def new_user(username,pswrd):
+    newUser = []
+    charList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","-","_","=","+","`","~","{","}","[","]","|","\\","\'","\"",":",";","<",">",",",".","?","/"," "]
+    tempCharList = []
+    for ele in charList:
+        tempCharList.append(ele)
+    randomCharList = []
+    for item in range(len(tempCharList)):
+        randomCharList.append(random.choice(tempCharList))
+        tempCharList.remove(randomCharList[item])
+    keyDict = {"a":"","b":"","c":"","d":"","e":"","f":"","g":"","h":"","i":"","j":"","k":"","l":"","m":"","n":"","o":"","p":"","q":"","r":"","s":"","t":"","u":"","v":"","w":"","x":"","y":"","z":"","A":"","B":"","C":"","D":"","E":"","F":"","G":"","H":"","I":"","J":"","K":"","L":"","M":"","N":"","O":"","P":"","Q":"","R":"","S":"","T":"","U":"","V":"","W":"","X":"","Y":"","Z":"","1":"","2":"","3":"","4":"","5":"","6":"","7":"","8":"","9":"","0":"","!":"","@":"","#":"","$":"","%":"","^":"","&":"","*":"","(":"",")":"","-":"","_":"","=":"","+":"","`":"","~":"","{":"","}":"","[":"","]":"","|":"","\\":"","\'":"","\"":"",":":"",";":"","<":"",">":"",",":"",".":"","?":"","/":""," ":""}
+    for item in range(len(charList)):
+        keyDict[charList[item]] = randomCharList[item]
+    unencryptedPass = []
+    for ele in range(len(pswrd)):
+         unencryptedPass.append(pswrd[ele])
+    encryptedPass = []
+    for ele in unencryptedPass:
+        encryptedPass.append(keyDict[ele])
+    encryptedPass = ''.join(encryptedPass)
+    newUser.append({"key":keyDict,"name":username,"password":encryptedPass,"fontColor":"default","fontBold":"no"})
+    diaryAtUser.append(newUser[0])
+    with open('diaryusers.json','w') as outfile:
+        jsonVar = json.dumps(diaryAtUser,indent=4)
+        outfile.write('{\n"users": ' + jsonVar + '\n}')
+    
 
 def user_intro():
     global userNum
     global userName
     global key
     global password
-    os.system('clear')
-    print("Have an account already? Type LOG IN".center(z) + "\n" + "Need to create an account? Type SIGN UP".center(z) + "\n")
-    logOrSign = input()
-    if logOrSign.lower() == "log in":
+    while True:
+        userNum = -2
         os.system('clear')
-        print("Username:     ".center(z))
-        userName = input()
-        print("Password:     ".center(z))
-        password = input()
-        for lcv in range(len(diaryAtUser)):
-            if userName == diaryusers["user"][lcv]['name']:
-                userNum = lcv
-        if userNum.isdigit():
-            if password == diaryusers["user"][userNum]['password']:
-                key = diaryusers["user"][userNum]['key']
-                fontColor = diaryusers["user"][userNum]['fontColor']
-                fontBold = diaryusers["user"][userNum]['fontBold']
-                
-            else:
-                userNum = "a"
-                
-    elif logOrSign.lower() == "sign up":
-        os.system('clear')
-        print("Create a username:     ".center(z))
-        userName = input()
-        print("Create a password:     ".center(z))
-        password = input()
-        for user in range(len(diaryAtUser)):
-            if userName == diaryusers["user"][user]["name"]: #checks to see if a username is already taken, if it is then local variable userNameMatch is not set to p to let the code know later that the username is taken
-                userNameMatch = "fail"
-            else:
-                pass
-        if len(userName) < 5 or len(userName) > 15:
+        print("Have an account already? Type LOG IN".center(z) + "\n" + "Need to create an account? Type SIGN UP".center(z) + "\n")
+        logOrSign = input()
+        if logOrSign.lower() == "log in":
             os.system('clear')
-            print(Fore.RED + Style.BRIGHT + "Username must be between 5 and 15 digits!".center(z) + Fore.RESET + Style.NORMAL)
-            time.sleep(3)
-            break
-        elif len(password) < 8 or len(password) > 24:
-            os.system('clear')
-            buddy_face_talkOption3()
-            print("")
-            print(Fore.RED + Style.BRIGHT + "Password must be between 8 and 24 digits!".center(z) + Fore.RESET + Style.NORMAL)
-            time.sleep(3)
-            break
-        elif userNameMatch == "fail":
-            os.system('clear')
-            buddy_face_talkOption3()
-            print("")
-            print(Fore.RED + Style.BRIGHT + "That username is already taken!".center(z) + Fore.RESET + Style.NORMAL)
-            time.sleep(3)
-            break
-        else:
-            userNameCorrect = "p"
-
-        if userNameCorrect == "p":
-            while True:
-                os.system('clear')
-                buddy_face_question()
-                print("")
-                print(f"Username: {userName}".center(z) + "\n" + (f"Password: {password[0]}{password[1]}" + ("*" * (len(password) - 3)) + f"{password[-1]}").center(z) + "\n" + "Does this information look correct?".center(z)) #neat little display of username and password with password being covered up
-                checkInfo = input()
-                if checkInfo.lower() == "yes":
-                    break
-                elif checkInfo.lower() == "no":
+            print("Username:     ".center(z))
+            userName = input()
+            print("Password:     ".center(z))
+            password = input()
+            for lcv in range(len(diaryAtUser)):
+                if userName == diaryusers["users"][lcv]['name']:
+                    userNum = lcv
+            if userNum >= 0:
+                encrypt(password)
+                if encryptedPass == diaryusers["users"][userNum]['password']:
+                    key = diaryusers["users"][userNum]['key']
+                    fontColor = diaryusers["users"][userNum]['fontColor']
+                    fontBold = diaryusers["users"][userNum]['fontBold']
                     break
                 else:
-                    pass
-            if checkInfo.lower() == "yes":
-                break
+                    print(Fore.RED + Style.BRIGHT + "Your username or password is incorrect".center(z) + Fore.RESET + Style.NORMAL)
+                    time.sleep(3)
+            else:
+                print(Fore.RED + Style.BRIGHT + "Your username or password is incorrect".center(z) + Fore.RESET + Style.NORMAL)
+                time.sleep(3)
+                    
+        elif logOrSign.lower() == "sign up":
+            while True:
+                userNameCorrect = 0
+                userNameMatch = 0
+                os.system('clear')
+                print("Create a username:     ".center(z))
+                userName = input()
+                print("Create a password:     ".center(z))
+                password = input()
+                for user in range(len(diaryAtUser)):
+                    if userName == diaryusers["users"][user]["name"]: #checks to see if a username is already taken, if it is then local variable userNameMatch is not set to p to let the code know later that the username is taken
+                        userNameMatch = "fail"
+                if len(userName) < 5 or len(userName) > 15:
+                    os.system('clear')
+                    print(Fore.RED + Style.BRIGHT + "Username must be between 5 and 15 digits!".center(z) + Fore.RESET + Style.NORMAL)
+                    time.sleep(3)
+                elif len(password) < 8 or len(password) > 24:
+                    os.system('clear')
+                    print(Fore.RED + Style.BRIGHT + "Password must be between 8 and 24 digits!".center(z) + Fore.RESET + Style.NORMAL)
+                    time.sleep(3)
+                elif userNameMatch == "fail":
+                    os.system('clear')
+                    print(Fore.RED + Style.BRIGHT + "That username is already taken!".center(z) + Fore.RESET + Style.NORMAL)
+                    userNameMatch = 0
+                    time.sleep(3)
+                else:
+                    userNameCorrect = "p"
+        
+                if userNameCorrect == "p":
+                    while True:
+                        os.system('clear')
+                        print(f"Username: {userName}".center(z) + "\n" + (f"Password: {password[0]}{password[1]}" + ("*" * (len(password) - 3)) + f"{password[-1]}").center(z) + "\n" + "Does this information look correct?".center(z)) #neat little display of username and password with password being covered up
+                        checkInfo = input()
+                        if checkInfo.lower() == "yes":
+                            new_user(userName,password)
+                            break
+                        elif checkInfo.lower() == "no":
+                            break
+                        else:
+                            pass
+                    if checkInfo.lower() == "yes":
+                        print(Style.BRIGHT + "You can now log in with your account!".center(z) + Style.NORMAL)
+                        time.sleep(3)
+                        break
 
+os.system('clear')
+user_intro()
+os.system('clear')
+print(Style.BRIGHT + "COMMAND OPTIONS: SETTINGS, START, HELP, QUIT".center(z) + Style.NORMAL)
+userInput = input("\n\n\n\n\n\n\n\n\n")
+while True:
+    if userInput.lower().strip() == "settings":
 
+    elif userInput.lower().strip() == "start":
 
+    elif userInput.lower().strip() == "help":
+        print("Commands for diary use:".center(z))
+        print("View: view the whole document")
+        print("Edit: open text editor to edit sentences")
+    elif userInput.lower().strip() == "quit":
