643e534e6290d529971a9a13c05cf36ef0436718
jackson-brick <jacksond.brick@gmail.com>
2025-01-07 19:53:22 +0000

    test

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
-entry = input()
+from colorama import Fore, Style, Back
 
-unencryptedEntry = []
-
-for ele in range(len(entry)):
-    if ele == "’" or ele == "‘":
-        unencryptedEntry.append("'")
-    elif ele == "“" or ele == "”":
-        unencryptedEntry.append("\"")
-    else:
-        unencryptedEntry.append(entry[ele])
-print(unencryptedEntry)
+dayname = "Mon"
+month = "Jan"
+daynum = " 7"
+strhour = "11"
+strmin = "37"
+period = "AM"
+print("----------------------------------------------------------------------------------------------------------")
+print("           " + Fore.BLACK + Style.DIM + "+=====================================================+" + Style.NORMAL)
+print("           |" + Back.LIGHTBLACK_EX + Style.BRIGHT + "      " + Fore.BLACK + "|" + Back.RESET + "      " + Fore.GREEN + "##       #####" + Fore.RESET + "                          |")
+print("           |     |      ###_____#########                        |            +----------------------+")
+print("           |     | ____/ __|_____|########                       |            |                      |")
+print(f"           |      /  ___/  |     |  ####                         |            |      {dayname} {month} {daynum}      |")
+print("           |        /      |     |                               |            |                      |")
+print("           |       /       |     |                               |            +----------------------+")
+print("           |\ /\ /\|/\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ |")
+print("           | |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  |  ||            +----------------------+")
+print(f"           |#####################################################|            |       {strhour}:{strmin} {period}       |")
+print("           |#####################################################|            +----------------------+")
+print("           +=====================================================+")
