1b5c7893998426689954365cc4807152edbe200f
jackson-brick <jacksond.brick@gmail.com>
2025-01-10 19:09:00 +0000

    update

--- randomsideprojects/tictactoe.py
+++ randomsideprojects/tictactoe.py
@@ -1,105 +1,121 @@
+import os
+from colorama import Back, Fore, Style
 
-print("+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+")
-print("|                                  |")
-print("|           Tic-Tac-Toe            |")
-print("|                                  |")
-print("|             Welcome              |")
-print("|                                  |")
-print("|                                  |")
-print("|-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-|")
-print("|                                  |")
-print("|                                  |")
-print("|  top left | top mid | top right  |")
-print("| -------------------------------- |")
-print("|     left  |  middle |  right     |")
-print("| -------------------------------- |")
-print("|  bot left | bot mid | bot right  |")
-print("|                                  |")
-print("+-+-+-+-+-+-+-+-+--+-+-+-+-+-+-+-+-+")
+z=os.get_terminal_size()
+z=z[0]
+print(Back.BLACK)
+os.system('clear')
+print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+".center(z))
+print("|                                     |".center(z))
+print("|            Tic-Tac-Toe              |".center(z))
+print("|                                     |".center(z))
+print("|              Welcome                |".center(z))
+print("|                                     |".center(z))
+print("|                                     |".center(z))
+print("|-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-|".center(z))
+print("|                                     |".center(z))
+print("|                                     |".center(z))
+print("|      1     |     2     |      3     |".center(z))
+print("| ----------------------------------- |".center(z))
+print("|      4     |     5     |      6     |".center(z))
+print("| ----------------------------------- |".center(z))
+print("|      7     |     8     |      9     |".center(z))
+print("|                                     |".center(z))
+print("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+".center(z))
 
 
-
+print("Press ENTER to begin!".center(z))
+input()
 
 
 winner = 0
 
-tl = " "
-tr = " "
-top = " "
-bl = " "
-br = " "
-bot = " "
-left = " "
-right = " "
-mid = " "
-
-print(f" {tl} | {top} | {tr} ")
-print("-----------")
-print(f" {left} | {mid} | {right} ")
-print("-----------")
-print(f" {bl} | {bot} | {br} ")
+tl = "1"
+tr = "3"
+top = "2"
+bl = "7"
+br = "9"
+bot = "8"
+left = "4"
+right = "6"
+mid = "5"
+os.system('clear')
+print("\n\n")
+print(f" {tl} | {top} | {tr} ".center(z))
+print("-----------".center(z))
+print(f" {left} | {mid} | {right} ".center(z))
+print("-----------".center(z))
+print(f" {bl} | {bot} | {br} ".center(z))
 
 while winner == 0:
-    move1 = input("Player 1's move: ")
-    move1 = move1.lower()
-    while move1 != "left" and move1 != "right" and move1 != "middle" and move1 != "top mid" and move1 != "top right" and move1 != "top left" and move1 != "bot mid" and move1 != "bot right" and move1 != "bot left":
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while not move1.isalpha:
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while move1 == "top left" and tl.isalpha():
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while move1 == "top mid" and top.isalpha():
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while move1 == "top right" and tr.isalpha():
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while move1 == "left" and left.isalpha():
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while move1 == "middle" and mid.isalpha():
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while move1 == "right" and right.isalpha():
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while move1 == "bot left" and bl.isalpha():
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while move1 == "bot mid" and bot.isalpha():
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    while move1 == "bot right" and br.isalpha():
-        print("That is not a valid move!")
-        move1 = input("Player 1's move: ")
-    if move1 == "top left":
+    print("Player 1's move: ".center(z))
+    move1 = input()
+    while move1 != "1" and move1 != "2" and move1 != "3" and move1 != "4" and move1 != "5" and move1 != "6" and move1 != "7" and move1 != "8" and move1 != "9":
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    while move1 == "1" and tl.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    while move1 == "2" and top.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    while move1 == "3" and tr.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    while move1 == "4" and left.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    while move1 == "5" and mid.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    while move1 == "6" and right.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    while move1 == "7" and bl.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    while move1 == "8" and bot.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    while move1 == "9" and br.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 1's move: ".center(z))
+        move1 = input()
+    if move1 == "1":
         tl = "X"
-    elif move1 == "top mid":
+    elif move1 == "2":
         top = "X"
-    elif move1 == "top right":
+    elif move1 == "3":
         tr = "X"
-    elif move1 == "left":
+    elif move1 == "4":
         left = "X"
-    elif move1 == "middle":
+    elif move1 == "5":
         mid = "X"
-    elif move1 == "right":
+    elif move1 == "6":
         right = "X"
-    elif move1 == "bot left":
+    elif move1 == "7":
         bl = "X"
-    elif move1 == "bot mid":
+    elif move1 == "8":
         bot = "X"
-    elif move1 == "bot right":
+    elif move1 == "9":
         br = "X"
 
-
-    print(f" {tl} | {top} | {tr} ")
-    print("-----------")
-    print(f" {left} | {mid} | {right} ")
-    print("-----------")
-    print(f" {bl} | {bot} | {br} ")
+    os.system('clear')
+    print("\n\n")
+    print(f" {tl} | {top} | {tr} ".center(z))
+    print("-----------".center(z))
+    print(f" {left} | {mid} | {right} ".center(z))
+    print("-----------".center(z))
+    print(f" {bl} | {bot} | {br} ".center(z))
 
     if tl == "X" and top == "X" and tr == "X":
         winner = 1
@@ -129,65 +145,73 @@ while winner == 0:
         winner = 3
         break
 
-    move2 = input("Player 2's move: ")
-    move2 = move2.lower()
-    while move2 != "left" and move2 != "right" and move2 != "middle" and move2 != "top mid" and move2 != "top right" and move2 != "top left" and move2 != "bot mid" and move2 != "bot right" and move2 != "bot left":
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while not move2.isalpha:
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while move2.lower() == "top left" and tl.isalpha():
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while move2.lower() == "top mid" and top.isalpha():
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while move2.lower() == "top right" and tr.isalpha():
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while move2.lower() == "left" and left.isalpha():
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while move2.lower() == "middle" and mid.isalpha():
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while move2.lower() == "right" and right.isalpha():
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while move2.lower() == "bot left" and bl.isalpha():
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while move2.lower() == "bot mid" and bot.isalpha():
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    while move2.lower() == "bot right" and br.isalpha():
-        print("That is not a valid move!")
-        move2 = input("Player 2's move: ")
-    if move2.lower() == "top left":
+    print("Player 2's move: ".center(z))
+    move2 = input()
+    while move2 != "1" and move2 != "2" and move2 != "3" and move2 != "4" and move2 != "5" and move2 != "6" and move2 != "7" and move2 != "8" and move2 != "9":
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    while move2.lower() == "1" and tl.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    while move2.lower() == "2" and top.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    while move2.lower() == "3" and tr.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    while move2.lower() == "4" and left.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    while move2.lower() == "5" and mid.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    while move2.lower() == "6" and right.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    while move2.lower() == "7" and bl.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    while move2.lower() == "8" and bot.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    while move2.lower() == "9" and br.isalpha():
+        print("That is not a valid move!".center(z))
+        print("Player 2's move: ".center(z))
+        move2 = input()
+    if move2.lower() == "1":
         tl = "O"
-    elif move2.lower() == "top mid":
+    elif move2.lower() == "2":
         top = "O"
-    elif move2.lower() == "top right":
+    elif move2.lower() == "3":
         tr = "O"
-    elif move2.lower() == "left":
+    elif move2.lower() == "4":
         left = "O"
-    elif move2.lower() == "middle":
+    elif move2.lower() == "5":
         mid = "O"
-    elif move2.lower() == "right":
+    elif move2.lower() == "6":
         right = "O"
-    elif move2.lower() == "bot left":
+    elif move2.lower() == "7":
         bl = "O"
-    elif move2.lower() == "bot mid":
+    elif move2.lower() == "8":
         bot = "O"
-    elif move2.lower() == "bot right":
+    elif move2.lower() == "9":
         br = "O"
-
-    print(f" {tl} | {top} | {tr} ")
-    print("-----------")
-    print(f" {left} | {mid} | {right} ")
-    print("-----------")
-    print(f" {bl} | {bot} | {br} ")
+    os.system('clear')
+    print("\n\n")
+    print(f" {tl} | {top} | {tr} ".center(z))
+    print("-----------".center(z))
+    print(f" {left} | {mid} | {right} ".center(z))
+    print("-----------".center(z))
+    print(f" {bl} | {bot} | {br} ".center(z))
 
     if tl == "O" and top == "O" and tr == "O":
         winner = 2
@@ -214,9 +238,26 @@ while winner == 0:
         winner = 2
         break
 
+os.system('clear')
+print(Fore.RED + Style.BRIGHT)
 if winner == 1:
-    print("Player 1 wins!")
+    print("\n\n\n")
+    print("0000  0      000  0   0 00000 0000     00     0     0 00000 0    0  0000  00".center(z))
+    print("0   0 0     0   0  0 0  0     0   0   0 0     0     0   0   00   0 0      00".center(z))
+    print("0000  0     00000   0   0000  0000      0     0  0  0   0   0 0  0  000   00".center(z))
+    print("0     0     0   0   0   0     0   0     0     0 0 0 0   0   0  0 0     0    ".center(z))
+    print("0     00000 0   0   0   00000 0   0   00000    0   0  00000 0   00 0000   00".center(z))
 elif winner == 2:
-    print("Player 2 wins!")
+    print("\n\n\n")
+    print("0000  0      000  0   0 00000 0000     000    0     0 00000 0    0  0000  00".center(z))
+    print("0   0 0     0   0  0 0  0     0   0   0   0   0     0   0   00   0 0      00".center(z))
+    print("0000  0     00000   0   0000  0000      00    0  0  0   0   0 0  0  000   00".center(z))
+    print("0     0     0   0   0   0     0   0   00      0 0 0 0   0   0  0 0     0    ".center(z))
+    print("0     00000 0   0   0   00000 0   0   00000    0   0  00000 0   00 0000   00".center(z))
 elif winner == 3:
-    print("It's a tie!")
+    print("\n\n\n")
+    print("00000 00000  0  0000     000     00000 00000 00000  00".center(z))
+    print("  0     0   0  0        0   0      0     0   0      00".center(z))
+    print("  0     0       000     00000      0     0   0000   00".center(z))
+    print("  0     0          0    0   0      0     0   0        ".center(z))
+    print("00000   0      0000     0   0      0   00000 00000  00".center(z))
