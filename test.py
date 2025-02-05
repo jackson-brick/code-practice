1b5f647c01f6e8ddaab08c51ff6f067af89580b1
jackson-brick <jacksond.brick@gmail.com>
2025-02-05 22:48:14 +0000

    cleaning up csv for unemployment

--- test.py
+++ test.py
@@ -6,10 +6,22 @@ with open('unemployment2023.csv','r') as file:
     for line in unemp:
         fullUnempCSV.append(line)
 
+removeList = []
 print(fullUnempCSV[100]["State"])
+state = fullUnempCSV[0]["State"]
 for line in fullUnempCSV:
-    if line["Attribute"][-2] != "2" and line["Attribute"][-1] != "3":
-        fullUnempCSV.remove(line)
+    if line["Attribute"][-2] != "2":
+        removeList.append(line)
+    elif line["Attribute"][-1] != "3":
+        removeList.append(line)        
+    if line["State"] != state:
+        print(f"Done with {state}")
+        state = line["State"]
+
+for value in removeList:
+    fullUnempCSV.remove(value)
+
+
 
 with open('unemployment2023.csv','w',newline='') as file:
     field_names = ["FIPS_Code","State","Area_Name","Attribute","Value"]
