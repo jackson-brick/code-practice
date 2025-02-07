adf1f6f9dda872f3d99cc83a5059cd37538634e1
jackson-brick <jacksond.brick@gmail.com>
2025-02-07 19:07:51 +0000

    asdasdasd

--- infograph.py
+++ infograph.py
@@ -27,7 +27,19 @@ with open('incomesince1958.csv','r') as file:
 unemp2023DATA = ""
 educ2023DATA = ""
 edToIncCADATA = pd.read_csv('calieducationtoincome2008-2014.csv')
+edToIncCADATA2008 = edToIncCADATA[edToIncCADATA["Year"]==2008].iloc[:,:]
+edToIncCADATA2008NHSD = edToIncCADATA2008[edToIncCADATA2008["Educational Attainment"] == "No high school diploma"].iloc[:,:]
+persInc = []
+for item in edToIncCADATA2008NHSD["Personal Income"]:
+    persInc.append(item)
+popCount = []
+for item in edToIncCADATA2008NHSD["Population Count"]:
+    popCount.append(item)
 incSince1958DATA = ""
 
-edToIncCADATA.plot(x="Personal Income",y="Population Count",title="Educational attainment")
-plot.savefig('figure.png')
+#edToIncCADATA2008NHSD.plot(x="Personal Income",y="Population Count",title="Educational attainment")
+plot.barh(persInc,popCount)
+plot.title("Income for People With No High School Diploma, 2008")
+plot.xlabel("# of People")
+plot.ylabel("Personal Income")
+plot.savefig('figure.png',bbox_inches="tight")
