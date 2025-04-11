4ef5cbcec1be35b47e122f5611c90e81e2e95324
jackson-brick <jacksond.brick@gmail.com>
2025-04-11 17:09:50 +0000

    wordle

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import csv
 import random
+from colorama import Fore, Style
+import os
+z = os.get_terminal_size()
+z = z[0]
 
 wordList = []
 with open('wordle.csv','r') as file:
@@ -8,6 +12,24 @@ with open('wordle.csv','r') as file:
         wordList.append(line)
 word = random.choice(wordList)
 word = word['1']+word['2']+word['3']+word['4']+word['5']
-player = input()
-newDict = {'1':player[0],'2':player[1],'3':player[2],'4':player[3],'5':player[4]}
-print(newDict in wordList)
+
+newWord = ["","","","",""]
+newWord[0] = "X"
+print(str(newWord))
+newWord[0] = (Fore.RED + Style.BRIGHT + "p" + Style.RESET_ALL)
+newWord += Fore.GREEN + Style.BRIGHT + "o" + Style.RESET_ALL
+print("control")
+print("".join(newWord))
+print("control")
+a = Fore.GREEN + Style.BRIGHT + "poped" + Style.RESET_ALL
+b = Fore.YELLOW + "poped" + Fore.RESET
+c = Style.DIM + "poped" + Style.RESET_ALL
+print(repr(a).center(z))
+print(a.center(z+12))
+print(len(a))
+print(repr(b).center(z))
+print(b.center(z+10))
+print(len(b))
+print(repr(c).center(z))
+print(c.center(z+8))
+print(len(c))
--- wordle.py
+++ wordle.py
@@ -7,34 +7,56 @@ z=z[0]
 os.system('clear')
 
 wordList = []
-with open('test.csv','r') as file:
+with open('wordle.csv','r') as file:
     wordle = csv.DictReader(file)
     for line in wordle:
         wordList.append(line)
 chosenWord = random.choice(wordList)
 chosenWord = chosenWord['1']+chosenWord['2']+chosenWord['3']+chosenWord['4']+chosenWord['5']
+count = [0,0,0,0,0,0]
 display = ["_ _ _ _ _","_ _ _ _ _","_ _ _ _ _","_ _ _ _ _","_ _ _ _ _","_ _ _ _ _"]
 
-for lcv in range(6):
-    for i in display:
-        print(i.center(z))
-        print("")
-    player = input()
-    if player.lower() == chosenWord:
-        os.system('clear')
-        print("You Win!".center(z))
-        quit()
-    if len(player) == 5:
-        playerDict = {'1':player[0],'2':player[1],'3':player[2],'4':player[3],'5':player[4]}
-        if playerDict in wordList:
-            for char in range(len(player)):
-                if chosenWord[char] == player[char].lower():
-                    display[char] = player[char].upper()
+for lcv in range(7):
+    while True:
+        print(chosenWord.center(z))
+        for i in range(6):
+            print(display[i].center(z+count[i]))
+            print(count[i])
+            print("")
+        player = input()
+        player = player.lower().strip()
+        if player == chosenWord:
+            os.system('clear')
+            print("You Win!".center(z))
+            quit()
+        if len(player) == 5:
+            playerDict = {'1':player[0],'2':player[1],'3':player[2],'4':player[3],'5':player[4]}
+            if playerDict in wordList:
+                playerList = list(player)
+                chosenWordList = list(chosenWord)
+                newPlayerList = ["","","","",""]
+                for char in range(len(player)):
+                    if chosenWordList[char] == playerList[char]:
+                        newPlayerList[char] = (Fore.GREEN + Style.BRIGHT + playerList[char].upper()+Style.RESET_ALL+" ")
+                        chosenWordList[char] = "!!"
+                        count[lcv]+=12
+                for char in range(len(player)):
+                    if playerList[char] in chosenWordList:
+                        newPlayerList[char] = (Fore.YELLOW + playerList[char].upper()+Fore.RESET +" ")
+                        chosenWordList[char] = "!!"
+                        count[lcv] += 10
+                    else:
+                        newPlayerList[char] = (Style.DIM + playerList[char].upper()+Style.RESET_ALL+" ")
+                        count[lcv] += 8
+                display[lcv] = "".join(newPlayerList)
+                break
+            else:
+                os.system('clear')
+                print(Fore.RED + Style.BRIGHT + "Word not in word list")
+                print(Style.RESET_ALL)
         else:
             os.system('clear')
-        print(Fore.RED + Style.BRIGHT + "Word not in word list")
-        print(Style.RESET_ALL)
-    else:
-        os.system('clear')
-        print(Fore.RED + Style.BRIGHT + "Word not in word list")
-        print(Style.RESET_ALL)
+            print(Fore.RED + Style.BRIGHT + "Word not in word list")
+            print(Style.RESET_ALL)
+    if lcv == 7:
+        print("Maybe next time!".center(z))
