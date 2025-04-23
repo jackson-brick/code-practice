24cfa9c69319fff9a4b7a2307dac10373b072277
jackson-brick <jacksond.brick@gmail.com>
2025-04-23 00:47:44 +0000

    tkinter doesnt work?

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 import tkinter as Tkinter
 import os
 
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using :0.0')
-    os.environ.__setitem__('DISPLAY', ':0.0')
+
+os.environ.__setitem__('DISPLAY', '0.0')
 master = Tkinter.Tk()
 master.title("tester")
 master.geometry("300x100")
