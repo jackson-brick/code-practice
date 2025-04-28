04cd0c2edfb47ee123ce4484852c002fa726bbec
jackson-brick <jacksond.brick@gmail.com>
2025-04-28 22:08:28 +0000

    adadasd

--- passwordgen.py
+++ passwordgen.py
@@ -5,6 +5,7 @@ z=os.get_terminal_size()[0]
 
 name = ""
 
+
 while True:
     os.system('clear')
     print("Are you using a website or an app?".center(z))
@@ -17,11 +18,26 @@ while True:
         break
 
 while True:
+    firstDot = 0
     os.system('clear')
     if webApp == "website":
         print("Enter the full url of the website you need the password for")
         website = input()
-        if website.count(".") < 2:
+        if website.count(".") == 1:
             for i in website:
+                if firstDot == 0 and i != ".":
+                    name += i
+                elif i == ".":
+                    firstDot += 1
         elif website.count(".") == 2:
+            for i in website:
+                if firstDot == 1 and i != ".":
+                    name += i
+                elif i == ".":
+                    firstDot += 1
+        else:
+            print("That is not a url. Press ENTER to try again.".center(z))
+            input()
+    print(name)
+    input()
 
