75b2b64238a1ce551f5b3c0607e0533ff43eb98d
jackson-brick <jacksond.brick@gmail.com>
2025-05-06 18:16:27 +0000

    done with login and signup, just debugging needed

--- resumeplease project/resumeplease.py
+++ resumeplease project/resumeplease.py
@@ -16,32 +16,49 @@ with open('resumeplease project/rpusers.csv','r') as file:
 
 intro_phrases = []
 
-def login(username, password):
+def login(username, password,forgot):
+    global identifiedUser
     for user in users:
         if user['username'] == username:
             key = user['key']
             decrypted = []
             for i in password:
                 decrypted.append(int(ord(i))*key)
-            if user['password'] == decrypted:
-                global identifiedUser
-                identifiedUser = user
-                users.remove(user)
-                return True
+            if forgot:
+                if user['phone'] == decrypted:
+                    identifiedUser = user
+                    users.remove(user)
+                    os.system('clear')
+                    actualPass = []
+                    for i in user['password']:
+                        actualPass.append(chr(int(i/key)))
+                    print(f"Your password is {"".join(actualPass)}".center(z))
+                    print("Press ENTER to continue".center(z))
+                    input()
+                    return True
+            else:
+                if user['password'] == decrypted:
+                    identifiedUser = user
+                    users.remove(user)
+                    return True
     return False
 
 def logout(user):
-    users.append(identifiedUser)
+    users.append(user)
     with open('rpusers.csv','w') as file:
         field_names = ["username","password","key","phone","checkpoint","iterations"]
         writer = csv.DictWriter(file,fieldnames=field_names)
 
 def new_user(username,password,key,phone):
     password = list(password)
+    phone = list(phone)
     encrypted = []
+    hiddenPhone = []
     for i in password:
         encrypted.append(int(ord(i))*key)
-    users.append({'username':username,'password':encrypted,'key':key,'phone':phone,'checkpoint':0,'iteration':0})
+    for i in phone:
+        hiddenPhone.append(int(ord(i))*key)
+    users.append({'username':username,'password':encrypted,'key':key,'phone':hiddenPhone,'checkpoint':0,'iteration':0})
 
 while True:
     os.system('clear')
@@ -50,69 +67,112 @@ while True:
     print("Don't have an account? Type SIGNUP to create one!".center(z))
     enter = input().lower().strip().replace(" ","")
     if enter == "login":
+        forgot = False
         os.system('clear')
         print("Type your username".center(z))
         username=input()
-        print("Type your password".center(z))
+        print("")
+        print("Type your password (If you forgot your password, type FORGOT)".center(z))
         password = input()
-        if login(username,password):
-            break
-        else:
-            for i in range(3):
-                os.system('clear')
+        try:
+            if password.lower().strip().replace(" ","") == "forgot":
                 print("")
-                print("INCORRECT USERNAME OR PASSWORD".center(z))
-                print("")
-                print(f"Returning to welcome screen in {3-i}".center(z))
-                time.sleep(1)
-    elif enter == "signup":
-        userInList = False
-        os.system('clear')
-        print("Create a username".center(z))
-        username = input()
-        for i in users:
-            if i['username'].lower().strip().replace(" ","") == username.lower().strip().replace(" ",''):
-                userInList == True
-        if not userInList:
-            print("Create a password (Minimum 5 characters, maximum 16)".center(z))
-            password = input()
-            if len(password) >= 5 and len(password) <=16:
-                print("Enter your phone number (used if you forget your password)".center(z))
-                phone = input()
-                if phone.isdigit():
-                    key = random.randint(12345,99999)
-                    new_user(username,password,key)
-                    for i in range(3):
-                        os.system('clear')
-                        print("")
-                        print("Now login on the welcome screen!".center(z))
-                        print("")
-                        print(f"{3-i}".center(z))
-                        time.sleep(1)
-                else:
-                    for i in range(3):
-                        os.system('clear')
-                        print("")
-                        print("Your phone is only numbers, please sign up again".center(z))
-                        print("")
-                        print(f"{3-i}".center(z))
-                        time.sleep(1)
+                print("Enter the phone number associated with your account".center(z))
+                phone = input().strip().replace("-","").replace(" ","")
+                forgot = True
+        except:
+            pass
+
+        if forgot:
+            if login(username,phone,forgot):
+                break
             else:
                 for i in range(3):
                     os.system('clear')
                     print("")
-                    print("Your password must be between 5 and 16 characters, please sign up again".center(z))
+                    print("INCORRECT USERNAME OR PASSWORD".center(z))
+                    print("")
+                    print(f"Returning to welcome screen in {3-i}".center(z))
+                    time.sleep(1)
+        else:        
+            if login(username,password,forgot):
+                break
+            else:
+                for i in range(3):
+                    os.system('clear')
+                    print("")
+                    print("INCORRECT USERNAME OR PASSWORD".center(z))
+                    print("")
+                    print(f"Returning to welcome screen in {3-i}".center(z))
+                    time.sleep(1)
+    elif enter == "signup":
+        while True:
+            userInList = False
+            os.system('clear')
+            print("Create a username".center(z))
+            username = input()
+            for i in users:
+                if i['username'].lower().strip().replace(" ","") == username.lower().strip().replace(" ",''):
+                    userInList == True
+            if not userInList:
+                print("Create a password (Minimum 5 characters, maximum 16)".center(z))
+                password = input()
+                if len(password) >= 5 and len(password) <=16:
+                    print("Enter your phone number (used if you forget your password)".center(z))
+                    phone = input().strip().replace(" ","").replace("-","")
+                    if phone.isdigit():
+                        infoValidCheck = False
+                        while True:
+                            os.system('clear')
+                            print(f"Username: {username}".center(z))
+                            print(f"Password: {password[0]}...{password[-3]}{password[-2]}{password[-1]}".center(z))
+                            print(f"Phone #: {phone}".center(z))
+                            print("")
+                            print("Does this information look correct?")
+                            infoValid = input().lower().strip().replace(" ","")
+                            if infoValid == "yes":
+                                infoValidCheck = True
+                                key = random.randint(12345,99999)
+                                new_user(username,password,key,phone)
+                                for i in range(3):
+                                    os.system('clear')
+                                    print("")
+                                    print("Now login on the welcome screen!".center(z))
+                                    print("")
+                                    print(f"{3-i}".center(z))
+                                    time.sleep(1)
+                                break
+                            elif infoValid == "no":
+                                break
+                        if infoValidCheck == True:
+                            break
+                    else:
+                        for i in range(3):
+                            os.system('clear')
+                            print("")
+                            print("Your phone is only numbers, please sign up again".center(z))
+                            print("")
+                            print(f"{3-i}".center(z))
+                            time.sleep(1)
+                        break
+                else:
+                    for i in range(3):
+                        os.system('clear')
+                        print("")
+                        print("Your password must be between 5 and 16 characters, please sign up again".center(z))
+                        print("")
+                        print(f"{3-i}".center(z))
+                        time.sleep(1)
+                    break
+            else:
+                for i in range(3):
+                    os.system('clear')
+                    print("")
+                    print("That username is taken, please sign up again".center(z))
                     print("")
                     print(f"{3-i}".center(z))
                     time.sleep(1)
-        else:
-            for i in range(3):
-                os.system('clear')
-                print("")
-                print("That username is taken, please sign up again".center(z))
-                print("")
-                print(f"{3-i}".center(z))
-                time.sleep(1)
+                break
 
 
 
@@ -122,4 +182,4 @@ print(identifiedUser)
 print(users)
 input()
 
-
+logout(identifiedUser)
--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-word = input("Password to be encrypted: ")
-
-print(list(word))
+import time
 
 
-
+input("Type something: ")
+for i in range(4):
+    print("Time is ticking...")
+    print(4-i)
+    time.sleep(1)
+test = input("Type the word fart: ")
+if test == "fart":
+    print("You passed!")
+else:
+    print("Test failed.")
