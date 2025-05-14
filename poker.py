68d74d95ec45b47a559ddff7d9e0a1fea7378450
jackson-brick <jacksond.brick@gmail.com>
2025-05-14 22:07:36 +0000

    cards work

--- poker.py
+++ poker.py
@@ -1,8 +1,104 @@
 import time
 import os
+import random
 z = os.get_terminal_size()[0]
 os.system('clear')
 
+d1 = " /\\ "
+d2 = "|  |"
+d3 = " \\/ "
+  
+c1 = " () "
+c2 = "()()"
+c3 = " || "
 
-deck = ['2 of Hearts', '3 of Hearts', '4 of Hearts', '5 of Hearts', '6 of Hearts', '7 of Hearts', '8 of Hearts', '9 of Hearts', '10 of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts', 'Ace of Hearts', '2 of Diamonds', '3 of Diamonds', '4 of Diamonds', '5 of Diamonds', '6 of Diamonds', '7 of Diamonds', '8 of Diamonds', '9 of Diamonds', '10 of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds', 'Ace of Diamonds', '2 of Clubs', '3 of Clubs', '4 of Clubs', '5 of Clubs', '6 of Clubs', '7 of Clubs', '8 of Clubs', '9 of Clubs', '10 of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs', 'Ace of Clubs', '2 of Spades', '3 of Spades', '4 of Spades', '5 of Spades', '6 of Spades', '7 of Spades', '8 of Spades', '9 of Spades', '10 of Spades', 'Jack of Spades', 'Queen of Spades', 'King of Spades', 'Ace of Spades']
+h1 = "/\\/\\"
+h2 = "\\  /"
+h3 = " \\/ "
 
+s1 = " /\\ "
+s2 = "(  )"
+s3 = " || "
+
+
+deck = ['2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', '10H', 'JH', 'QH', 'KH', 'AH', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', '10D', 'JD', 'QD', 'KD', 'AD', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', '10C', 'JC', 'QC', 'KC', 'AC', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS']
+chosenCards = []
+
+def displayCard(value1,suit1,value2,suit2,ten0,ten1):
+    if ten0:
+        al = value1
+        ar = value1
+    else:
+        al = value1 + " "
+        ar = " " + value1
+    if ten1:
+        bl = value2
+        br = value2
+    else:
+        bl = value2 + " "
+        br = " " + value2
+
+    
+    if suit1 == "D":
+        row1c1 = d1
+        row2c1 = d2
+        row3c1 = d3
+    elif suit1 == "H":
+        row1c1 = h1
+        row2c1 = h2
+        row3c1 = h3
+    elif suit1 == "C":
+        row1c1 = c1
+        row2c1 = c2
+        row3c1 = c3
+    elif suit1 == "S":
+        row1c1 = s1
+        row2c1 = s2
+        row3c1 = s3
+    if suit2 == "D":
+        row1c2 = d1
+        row2c2 = d2
+        row3c2 = d3
+    elif suit2 == "H":
+        row1c2 = h1
+        row2c2 = h2
+        row3c2 = h3
+    elif suit2 == "C":
+        row1c2 = c1
+        row2c2 = c2
+        row3c2 = c3
+    elif suit2 == "S":
+        row1c2 = s1
+        row2c2 = s2
+        row3c2 = s3
+    print(f"+------------+       +------------+".center(z))
+    print(f"| {al}      {ar} |       | {bl}      {br} |".center(z))
+    print(f"|            |       |            |".center(z))
+    print(f"|    {row1c1}    |       |    {row1c2}    |".center(z))
+    print(f"|    {row2c1}    |       |    {row2c2}    |".center(z))
+    print(f"|    {row3c1}    |       |    {row3c2}    |".center(z))
+    print(f"|            |       |            |".center(z))
+    print(f"| {al}      {ar} |       | {bl}      {br} |".center(z))
+    print(f"+------------+       +------------+".center(z))
+
+
+def printPlayerCards():
+    if "10" in playerCards[0]:
+        if "10" in playerCards[1]:
+            displayCard(playerCards[0][:2],playerCards[0][-1],playerCards[1][:2],playerCards[1][-1],True,True)
+        else:
+            displayCard(playerCards[0][:2],playerCards[0][-1],playerCards[1][0],playerCards[1][-1],True,False)
+    elif "10" in playerCards[1]:
+        displayCard(playerCards[0][0],playerCards[0][-1],playerCards[1][:2],playerCards[1][-1],False,True)
+    else:
+        displayCard(playerCards[0][0],playerCards[0][-1],playerCards[1][0],playerCards[1][-1],False,False)
+playerCards = []
+for i in range(2):
+    x = random.choice(deck)
+    deck.remove(x)
+    playerCards.append(x)
+
+os.system('clear')
+print("Your cards".center(z))
+print("")
+printPlayerCards()
