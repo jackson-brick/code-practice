170a6bc62594e04da81e8822d8aeff8e66673207
jackson-brick <jacksond.brick@gmail.com>
2025-09-18 06:33:45 +0000

    Mostly working in terms of math and display, need to figure out ACE logic

--- blackjack.py
+++ blackjack.py
@@ -15,16 +15,25 @@ def get_name_of_card(suit,number):
     else:
         return f"{number} of {suit}"
 
-def get_card_value(card):
+def get_card_value(card,score):
     if card[:3].lower() == "ace":
-        return -1
+        if score+11 <= 21:
+            return 11
+        else:
+            return 1
     elif card[:4] == "Jack" or card[:4] == "King" or card[:5] == "Queen":
         return 10
     elif card[1] == " ":
         return int(card[0])
     else:
         return int(card[:2])
-    
+
+def two_aces(card1,card2):
+    if card1[:3].lower() == "ace":
+        if card2[:3].lower() == "ace":
+            return True
+    return False
+
 def deal_card():
     x=random.choice(deckList)
     deckList.remove(x)
@@ -38,27 +47,38 @@ def is_vowel(card):
 
 def card_display(hand,phase):
     numCards = len(hand)
-    print("-------  " * numCards)
+    topEdge = "-------  " * numCards
+    topLine = ""
+    midLine = ""
+    bottomLine = ""
+    bottomEdge = "-------  " * numCards
+    print(topEdge.center(z))
     if phase == "open":
         for i in range(numCards):
-            print(f"|   {str(get_value_display(hand[i],"top"))}|  ", end="")
-        print("\n"+"|     |  " * numCards)
+            topLine += f"|   {str(get_value_display(hand[i],"top"))}|  "
+        print(topLine.center(z))
+        midLine = "|     |  " * numCards
+        print(midLine.center(z))
         for i in range(numCards):
-            print(f"|{str(get_value_display(hand[i],"bottom"))}   |  ", end="")
+            bottomLine += f"|{str(get_value_display(hand[i],"bottom"))}   |  "
+        print(bottomLine.center(z))
     elif phase=="hidden":
         for i in range(numCards):
             if i == 0:
-                print("|/////|  ",end="")
+                topLine += "|/////|  "
             else:
-                print(f"|   {str(get_value_display(hand[i],"top"))}|  ", end="")
-        print("\n"+"|/////|  " + "|     |  " * (numCards-1))
+                topLine += f"|   {str(get_value_display(hand[i],"top"))}|  "
+        print(topLine.center(z))
+        midLine = "|/////|  " + "|     |  " * (numCards-1)
+        print(midLine.center(z))
         for i in range(numCards):
             if i == 0:
-                print("|/////|  ",end="")
+                bottomLine += "|/////|  "
             else:
-                print(f"|{str(get_value_display(hand[i],"bottom"))}   |  ", end="")
+                bottomLine += f"|{str(get_value_display(hand[i],"bottom"))}   |  "
+        print(bottomLine.center(z))
 
-    print("\n"+"-------  " * numCards)
+    print(bottomEdge.center(z))
 
 def get_value_display(card,side):
     if side == "top":
@@ -111,29 +131,64 @@ for i in range(numDecks):
             deckList.append(get_name_of_card(suit,num))
 
 playerCards = []
+playerScore = 0
 dealerCards = []
+dealerScore = 0
+random.shuffle(deckList)
 playerCards.append(deal_card())
 dealerCards.append(deal_card())
 playerCards.append(deal_card())
 dealerCards.append(deal_card())
-print("You are dealt a" + is_vowel(playerCards[0]) + playerCards[0])
+playerScore += get_card_value(playerCards[0],playerScore)
+playerScore += get_card_value(playerCards[1],playerScore)
+dealerScore += get_card_value(dealerCards[0],playerScore)
+dealerScore += get_card_value(dealerCards[1],playerScore)
+print(f"You are dealt a{is_vowel(playerCards[0])}{playerCards[0]}".center(z))
 print("")
 time.sleep(1.7)
-print("The dealer deals himself a card face down")
+print("The dealer deals himself a card face down".center(z))
 print("")
 time.sleep(1.7)
-print("You are dealt a" + is_vowel(playerCards[1]) + playerCards[1])
+print(f"You are dealt a{is_vowel(playerCards[1])}{playerCards[1]}".center(z))
 print("")
 time.sleep(1.7)
-print("The dealer deals himself a" + is_vowel(dealerCards[1]) + dealerCards[1])
+print(f"The dealer deals himself a{is_vowel(dealerCards[1])}{dealerCards[1]}".center(z))
 print("")
+time.sleep(1.7)
+print("Press ENTER to start the game".center(z))
+input()
 
-os.system('clear')
-print("Your cards:\n")
-card_display(playerCards,"open")
-print("\nDealer's Cards:")
-card_display(dealerCards,"hidden")
+result = ""
+while result == "":
+    os.system('clear')
+    print(f"Your cards:        Your score: {playerScore}".center(z))
+    print("")
+    card_display(playerCards,"open")
+    print("")
+    print(f"Dealer's Cards:".center(z))
+    card_display(dealerCards,"hidden")
 
-print("")
-print("What would you like to do?".center(z))
-print("HIT   |   STAND   |   DOUBLE")
+    print("")
+    print("What would you like to do?".center(z))
+    if get_card_value(playerCards[0],0) == get_card_value(playerCards[1],0) or two_aces(playerCards[0],playerCards[1]):
+        print("HIT   |   STAND   |   DOUBLE   |   SURRENDER   |   SPLIT".center(z))
+    else:
+        print("HIT   |   STAND   |   DOUBLE   |   SURRENDER".center(z))
+    action = input().lower()
+    if action == "hit":
+        playerCards.append(deal_card())
+        playerScore += get_card_value(playerCards[-1],playerScore)
+    elif action == "stand":
+        print("stand")
+    elif action == "double":
+        print("double down")
+    elif action == "split" and get_card_value(playerCards[0],0) == get_card_value(playerCards[1],0) or action == "split" and two_aces(playerCards[0],playerCards[1]):
+        print("split")
+
+    if playerScore > 21:
+        for i in playerCards:
+            if i[:3].lower() == "ace":
+                FINSIH LATER
+        result = "dealerWin"
+    if dealerScore > 21:
+        result = "playerWin"
