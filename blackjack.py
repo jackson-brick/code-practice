5dbf83822b54777cbfcf5189029723c545f2e9e8
jackson-brick <jacksond.brick@gmail.com>
2025-09-23 17:11:11 +0000

    keep working on ending options

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,10 @@
 import random
-import time
 import os
+import time
+from colorama import Fore,Style
 z = os.get_terminal_size()[0]
 os.system('clear')
+animations = True
 def get_name_of_card(suit,number):
     if number == 1:
         return f"Ace of {suit}"
@@ -15,18 +17,15 @@ def get_name_of_card(suit,number):
     else:
         return f"{number} of {suit}"
 
-def get_card_value(card,score):
+def get_card_value(card):
     if card[:3].lower() == "ace":
-        if score+11 <= 21:
-            return 11
-        else:
-            return 1
+        return -1
     elif card[:4] == "Jack" or card[:4] == "King" or card[:5] == "Queen":
         return 10
     elif card[1] == " ":
         return int(card[0])
     else:
-        return int(card[:2])
+        return 10
 
 def two_aces(card1,card2):
     if card1[:3].lower() == "ace":
@@ -52,33 +51,33 @@ def card_display(hand,phase):
     midLine = ""
     bottomLine = ""
     bottomEdge = "-------  " * numCards
-    print(topEdge.center(z))
+    print(topEdge.strip().center(z))
     if phase == "open":
         for i in range(numCards):
             topLine += f"|   {str(get_value_display(hand[i],"top"))}|  "
-        print(topLine.center(z))
+        print(topLine.strip().center(z))
         midLine = "|     |  " * numCards
-        print(midLine.center(z))
+        print(midLine.strip().center(z))
         for i in range(numCards):
             bottomLine += f"|{str(get_value_display(hand[i],"bottom"))}   |  "
-        print(bottomLine.center(z))
+        print(bottomLine.strip().center(z))
     elif phase=="hidden":
         for i in range(numCards):
-            if i == 0:
+            if i == 1:
                 topLine += "|/////|  "
             else:
                 topLine += f"|   {str(get_value_display(hand[i],"top"))}|  "
-        print(topLine.center(z))
-        midLine = "|/////|  " + "|     |  " * (numCards-1)
-        print(midLine.center(z))
+        print(topLine.strip().center(z))
+        midLine = "|     |  " + "|/////|  " * (numCards-1)
+        print(midLine.strip().center(z))
         for i in range(numCards):
-            if i == 0:
+            if i == 1:
                 bottomLine += "|/////|  "
             else:
                 bottomLine += f"|{str(get_value_display(hand[i],"bottom"))}   |  "
-        print(bottomLine.center(z))
+        print(bottomLine.strip().center(z))
 
-    print(bottomEdge.center(z))
+    print(bottomEdge.strip().center(z))
 
 def get_value_display(card,side):
     if side == "top":
@@ -112,10 +111,188 @@ def calculate_score(hand):
     aceCount=0
     score = 0
     for i in hand:
-        if i[:3].lower() == "ace":
-            aceCount +=1
+        if get_card_value(i) == -1:
+            aceCount+=1
         else:
             score += get_card_value(i)
+    if aceCount >0:
+        score += aceCount
+        if score + 10<=21:
+            score += 10
+
+    return score
+
+def check_card_animation(dealerHand):
+    os.system('clear')
+    print("_"*z)
+    card_display(dealerHand,"hidden")
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "-------" + "_"*int(((z/2)-8)))
+    print("-------  |/////|".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|  |/////|".center(z))
+    print("|     |  |/////|".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |  -------".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "|/////|" + "_"*int(((z/2)-8)))
+    print("-------  |/////|".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|  |/////|".center(z))
+    print("|     |  -------".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "|/////|" + "_"*int(((z/2)-8)))
+    print("-------  |/////|".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|  -------".center(z))
+    print("|     |         ".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "|/////|" + "_"*int(((z/2)-8)))
+    print("-------  -------".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|         ".center(z))
+    print("|     |         ".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "-------" + "_"*int(((z/2)-8)))
+    print("-------         ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|         ".center(z))
+    print("|     |         ".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*z)
+    print("-------         ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|         ".center(z))
+    print("|     |         ".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(2)
+    os.system('clear')
+    print("_"*z)
+    print("-------         ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|         ".center(z))
+    print("|     |         ".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "-------" + "_"*int(((z/2)-8)))
+    print("-------         ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|         ".center(z))
+    print("|     |         ".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "|/////|" + "_"*int(((z/2)-8)))
+    print("-------  -------".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|         ".center(z))
+    print("|     |         ".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "|/////|" + "_"*int(((z/2)-8)))
+    print("-------  |/////|".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|  -------".center(z))
+    print("|     |         ".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "|/////|" + "_"*int(((z/2)-8)))
+    print("-------  |/////|".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|  |/////|".center(z))
+    print("|     |  -------".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |         ".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*int(((z/2)+1)) + "-------" + "_"*int(((z/2)-8)))
+    print("-------  |/////|".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|  |/////|".center(z))
+    print("|     |  |/////|".center(z))
+    print(f"|{str(get_value_display(dealerCards[0],"bottom"))}   |  -------".center(z))
+    print("-------         ".center(z))
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*z)
+    card_display(dealerHand,"hidden")
+    print("The dealer is checking his hidden card for a blackjack".center(z))
+
+def hidden_flip_animation():
+    print("_"*z)
+    card_display(dealerCards,"hidden")
+    print(f"Dealer's Cards".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*z)
+    print("-------   ----- ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|   |///| ".center(z))
+    print("|     |   |///| ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"bottom"))}|   |///| ".center(z))
+    print("-------   ----- ".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*z)
+    print("-------    ---  ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|    |/|  ".center(z))
+    print("|     |    |/|  ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"bottom"))}|    |/|  ".center(z))
+    print("-------    ---  ".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*z)
+    print("-------     -   ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|     |   ".center(z))
+    print("|     |     |   ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"bottom"))}|     |   ".center(z))
+    print("-------     -   ".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*z)
+    print("-------    ---  ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|    | |  ".center(z))
+    print("|     |    | |  ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"bottom"))}|    | |  ".center(z))
+    print("-------    ---  ".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+    print("_"*z)
+    print("-------   ----- ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"top"))}|   | {str(get_value_display(dealerCards[1],"top"))}| ".center(z))
+    print("|     |   |   | ".center(z))
+    print(f"|   {str(get_value_display(dealerCards[0],"bottom"))}|   |{str(get_value_display(dealerCards[1],"bottom"))} | ".center(z))
+    print("-------   ----- ".center(z))
+    time.sleep(0.3)
+    os.system('clear')
+
+def stand():
+    os.system('clear')
+
+    
+    
 
 print("Welcome to blackjack".center(z))
 while True:
@@ -141,64 +318,99 @@ for i in range(numDecks):
             deckList.append(get_name_of_card(suit,num))
 
 playerCards = []
-playerScore = 0
+
 dealerCards = []
-dealerScore = 0
 random.shuffle(deckList)
 playerCards.append(deal_card())
 dealerCards.append(deal_card())
 playerCards.append(deal_card())
 dealerCards.append(deal_card())
-playerScore += get_card_value(playerCards[0],playerScore)
-playerScore += get_card_value(playerCards[1],playerScore)
-dealerScore += get_card_value(dealerCards[0],playerScore)
-dealerScore += get_card_value(dealerCards[1],playerScore)
 print(f"You are dealt a{is_vowel(playerCards[0])}{playerCards[0]}".center(z))
 print("")
-time.sleep(1.7)
-print("The dealer deals himself a card face down".center(z))
+print(f"The dealer deals himself a{is_vowel(dealerCards[0])}{dealerCards[0]}".center(z))
 print("")
-time.sleep(1.7)
 print(f"You are dealt a{is_vowel(playerCards[1])}{playerCards[1]}".center(z))
 print("")
-time.sleep(1.7)
-print(f"The dealer deals himself a{is_vowel(dealerCards[1])}{dealerCards[1]}".center(z))
+print("The dealer deals himself a card face down".center(z))
 print("")
-time.sleep(1.7)
 print("Press ENTER to start the game".center(z))
 input()
 
 result = ""
+gameStart = True
 while result == "":
+    playerScore = calculate_score(playerCards)
+    dealerScore = calculate_score(dealerCards)
+    if playerScore > 21:
+        #Player score will only increase if player hits
+        result = "playerBust"
+        
+    if dealerScore > 21:
+        #Dealer score only increases if the player stands
+        result = "dealerBust"
+        break
+    if gameStart:
+        if get_card_value(dealerCards[0]) == 10 or get_card_value(dealerCards[0]) == -1:    
+            gameStart = False
+            os.system('clear')
+            if animations:
+                check_card_animation(dealerCards)
+            else:
+                print("The dealer is checking his hidden card for a blackjack".center(z))
+                time.sleep(0.5)
+            
+            if dealerScore == 21:
+                #This ending will flip the dealer's card and show their blackjack
+                result = "dealerGameStartBlackjack"
+                break
+            else:
+                print("The dealer does not have a blackjack".center(z))
+                print("Press ENTER to continue".center(z))
+                input()
     os.system('clear')
+    print("_"*z)
+
+    card_display(dealerCards,"hidden")
+    print(f"Dealer's Cards:".center(z))
+    print("\n\n\n")
+    
+    card_display(playerCards,"open")
     print(f"Your cards:        Your score: {playerScore}".center(z))
     print("")
-    card_display(playerCards,"open")
-    print("")
-    print(f"Dealer's Cards:".center(z))
-    card_display(dealerCards,"hidden")
-
-    print("")
+    
     print("What would you like to do?".center(z))
-    if get_card_value(playerCards[0],0) == get_card_value(playerCards[1],0) or two_aces(playerCards[0],playerCards[1]):
+    if get_card_value(playerCards[0]) == get_card_value(playerCards[1]):
         print("HIT   |   STAND   |   DOUBLE   |   SURRENDER   |   SPLIT".center(z))
     else:
         print("HIT   |   STAND   |   DOUBLE   |   SURRENDER".center(z))
-    action = input().lower()
+    if result == "":
+        action = input().lower()
+    else:
+        action = ""
     if action == "hit":
         playerCards.append(deal_card())
-        playerScore += get_card_value(playerCards[-1],playerScore)
     elif action == "stand":
-        print("stand")
+        stand()
     elif action == "double":
         print("double down")
-    elif action == "split" and get_card_value(playerCards[0],0) == get_card_value(playerCards[1],0) or action == "split" and two_aces(playerCards[0],playerCards[1]):
+    elif action == "split" and get_card_value(playerCards[0]) == get_card_value(playerCards[1]):
         print("split")
 
-    if playerScore > 21:
-        for i in playerCards:
-            if i[:3].lower() == "ace":
-                print("FINSIH LATER")
-        result = "dealerWin"
-    if dealerScore > 21:
-        result = "playerWin"
+if result == "playerBust":
+    #dealer wins
+    print("")
+    print(Fore.RED + Style.BRIGHT + "DEALER WINS".center(z))
+elif result == "dealerBust":
+    #player wins
+    print("")
+    print(Fore.GREEN + Style.BRIGHT + "YOU WIN".center(z))
+elif result == "dealerGameStartBlackjack":
+    if (get_card_value(playerCards[0])+get_card_value(playerCards[1])) != 21:
+        #dealer wins
+        print("")
+        print(Fore.RED + Style.BRIGHT + "DEALER WINS".center(z))
+    else:
+        #tie
+        print("")
+        print("")
+        print(Fore.YELLOW + Style.BRIGHT + "IT'S A TIE".center(z))
