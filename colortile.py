2be7e412685034856e06425d20cddab6ee4224f1
jackson-brick <jacksond.brick@gmail.com>
2025-11-05 06:06:03 +0000

    basic funcitonality for game seems to work; needs debugging to make sure it works for all cases, and needs levels to be added, and needs move counter

--- colortile.py
+++ colortile.py
@@ -1,12 +1,13 @@
 import os
+import time
 from colorama import Back,Style,Fore
 z = os.get_terminal_size()[0]
 
 def findY(backColorCount):
-    return (len(Back.RED + Back.RESET)*backColorCount)
+    return (int(len(Back.RED + Back.RESET)*backColorCount))
 
 def resetCells():
-    global row1Color,row2Color,row3Color,row4Color,row5Color,row6Color,row1,row2,row3,row4,row5,row6
+    global row1Color,row2Color,row3Color,row4Color,row5Color,row6Color,row1,row2,row3,row4,row5,row6,rowList
     row1Color = 0
     row2Color = 0
     row3Color = 0
@@ -20,12 +21,19 @@ def resetCells():
     row4 = ["   ","   ","   ","   ","   ","   "]
     row5 = ["   ","   ","   ","   ","   ","   "]
     row6 = ["   ","   ","   ","   ","   ","   "]
+    rowList = [row1,row2,row3,row4,row5,row6]
     
 def countColors(row):
     colorCount = 0
     for i in row:
-        if i != "   ":
+        if i != "   " and i != "XXX":
+            tempList = []
+            tempList.append(i)
             colorCount += 1
+
+            if str(tempList)[3:10] == "x1b[103" or str(tempList)[3:10] == "x1b[101":
+                colorCount += 0.1
+
     return colorCount
 
 def threeXThree():
@@ -94,43 +102,99 @@ def sixXSix():
     print(f"|{row6[0]}|{row6[1]}|{row6[2]}|{row6[3]}|{row6[4]}|{row6[5]}|".center(z+findY(row6Color)))
     print(f"+---+---+---+---+---+---+".center(z))
 
-def action(keyPress):
+def action(keyPress,width):
     keyPress = keyPress.lower().strip()
     if keyPress == "a":
-        shift("left")
+        shift("left",width)
     elif keyPress == "d":
-        shift("right")
+        shift("right",width)
     elif keyPress == "w":
-        shift("up")
+        shift("up",width)
     elif keyPress == "s":
-        shift("down")
+        shift("down",width)
 
-def shift(direction):
-    if direction == "up":
-        for i in range(len(row2)):
-            if row1[i] == "   ":
-                row1[i] = row2[i]
-                row2[i] = "   "
-            if row2[i] == "   ":
-                row2[i] = row3[i]
-                row3[i] = "   "
-            if row3[i] == "   ":
-                row3[i] = row4[i]
-                row4[i] = "   "
-            if row4[i] == "   ":
-                row4[i] = row5[i]
-                row5[i] = "   "
-            if row5[i] == "   ":
-                row5[i] = row6[i]
-                row6[i] = "   "
-    #elif direction == "down":
+def shift(direction,width):
+    
+    while True:
+        tempRow1 = []
+        tempRow2 = []
+        tempRow3 = []
+        tempRow4 = []
+        tempRow5 = []
+        tempRow6 = []
+        for i in row1:
+            tempRow1.append(i)
+        for i in row2:
+            tempRow2.append(i)
+        for i in row3:
+            tempRow3.append(i)
+        for i in row4:
+            tempRow4.append(i)
+        for i in row5:
+            tempRow5.append(i)
+        for i in row6:
+            tempRow6.append(i)
+        
+        tempRowList = [tempRow1,tempRow2,tempRow3,tempRow4,tempRow5,tempRow6]
+        if direction == "up":
+            for i in range(width-1):
+                for j in range(width):
+                    if rowList[i][j] == "   ":
+                        rowList[i][j] = rowList[i+1][j]
+                        rowList[i+1][j] = "   "
+                
+        elif direction == "down":
+            for i in range(width-1):
+                for j in range(width):
+                    if rowList[width-(i+1)][j] == "   ":
+                        rowList[width-(i+1)][j] = rowList[width-(i+2)][j]
+                        rowList[width-(i+2)][j] = "   "
 
-    #elif direction == "left":
+        elif direction == "left":
+            for i in range(width):
+                for j in range(width):
+                    if j!=0:
+                        if rowList[i][j-1] == "   ":
+                            rowList[i][j-1] = rowList[i][j]
+                            rowList[i][j] = "   "
 
-    #elif direction == "right":
+        elif direction == "right":
+            for i in range(width):
+                for j in range(width):
+                    if j!=width:
+                        if rowList[i][j+1] == "   ":
+                            rowList[i][j+1] = rowList[i][j]
+                            rowList[i][j] = "   "
+        
+        if tempRowList==rowList:
+            #print(tempRowList)
+            #print(rowList)
+            #input()
+            break
+
+def voidRest(width=0):
+    global row1, row2, row3, row4, row5, row6
+    rowList = [row1,row2,row3,row4,row5,row6]
+    for row in range(len(rowList)-width):
+        for cell in range(len(rowList[row])-width):
+            if rowList[row+width][cell+width] == "   ":
+                rowList[row+width][cell+width] = "XXX"
+    for row in range(len(rowList)-width):
+        for cell in range(len(rowList[row])-width):
+            if rowList[row+width][cell+width] == "EMT":
+                rowList[row+width][cell+width] = "   "
+
+def cleanBoard(width):
+    global row1,row2,row3,row4,row5,row6
+    rowList = [row1,row2,row3,row4,row5,row6]
+    for row in range(width):
+        for cell in range(width):
+            if rowList[row][cell] == "XXX":
+                rowList[row][cell] = "   "
 
 def levelOneSettings():
-    global row1,row2,row3
+    global row1,row2,row3,width
+    width = 3
     row1[0] = Back.RED + "   " + Back.RESET
     row1[1] = Back.RED + "   " + Back.RESET
     row1[2] = Back.RED + "   " + Back.RESET
@@ -139,6 +203,22 @@ def levelOneSettings():
     row2[2] = Back.YELLOW + "   " + Back.RESET
     row3[0] = Back.YELLOW + "   " + Back.RESET
     row3[1] = Back.YELLOW + "   " + Back.RESET
+    row2[1] = "EMT"
+    voidRest()
+
+
+def testSettings():
+    global row2
+    row2[1] = Back.RED + "   " + Back.RESET
+    row1[0] = "EMT"
+    row1[1] = "EMT"
+    row1[2] = "EMT"
+    row2[0] = "EMT"
+    row2[2] = "EMT"
+    row3[0] = "EMT"
+    row3[1] = "EMT"
+    row3[2] = "EMT"
+    voidRest()
 
 def aboveOrBelow(row,pos):
     if pos == "above":
@@ -165,21 +245,157 @@ def aboveOrBelow(row,pos):
             return row6
     return 0
 
+def getStringRow(cell):
+    return "row"+cell[1]
 
-def checkCells():
-    rowsList = [row1,row2,row3,row4,row5,row6]
+def checkCells(width):
     matchList = []
     trashList = []
-    for row in rowsList:
-        for i in range(len(row)):
-            matchList.append(row[i])
-            above = aboveOrBelow(row,"above")
-            if above != 0:
-                if above == row[i]:
-                    matchList.append(above)
+    delList = []
+    for row in range(width):
+        for cell in (range(width)):
+            if [row,cell] not in trashList:
+                if rowList[row][cell] != "   ":
+                    matchList.append([row,cell])
+                    try:
+                        above = rowList[row-1][cell]
+                        if above == "XXX":
+                            above = "DNE"
+                    except:
+                        above = "DNE"
+                    try:
+                        below = rowList[row+1][cell]
+                        if below == "XXX":
+                            below = "DNE"
+                    except:
+                        below = "DNE"
+                    try:
+                        left = rowList[row][cell-1]
+                        if left == "XXX":
+                            left = "DNE"
+                    except:
+                        left = "DNE"
+                    try:
+                        right = rowList[row][cell+1]
+                        if right == "XXX":
+                            right = "DNE"
+                    except:
+                        right = "DNE"
+                    if above == rowList[row][cell]:
+                        matchList.append([row-1,cell])
+                    if below == rowList[row][cell]:
+                        matchList.append([row+1,cell])
+                    if left == rowList[row][cell]:
+                        matchList.append([row,cell-1])
+                    if right == rowList[row][cell]:
+                        matchList.append([row,cell+1])
+                    if len(matchList) > 1:
+                        for lcv in matchList:
+                            if matchList.index(lcv) != 0:
+                                try:
+                                    above = rowList[lcv[0]-1][lcv[1]]
+                                    if above == "XXX":
+                                        above = "DNE"
+                                except:
+                                    above = "DNE"
+                                try:
+                                    below = rowList[lcv[0]+1][lcv[1]]
+                                    if below == "XXX":
+                                        below = "DNE"
+                                except:
+                                    below = "DNE"
+                                try:
+                                    left = rowList[lcv[0]][lcv[1]-1]
+                                    if left == "XXX":
+                                        left = "DNE"
+                                except:
+                                    left = "DNE"
+                                try:
+                                    right = rowList[lcv[0]][lcv[1]+1]
+                                    if right == "XXX":
+                                        right = "DNE"
+                                except:
+                                    right = "DNE"
+                                if above == rowList[lcv[0]][lcv[1]]:
+                                    if [lcv[0]-1,lcv[1]] not in matchList:
+                                        matchList.append([lcv[0]-1,lcv[1]])
+                                if below == rowList[lcv[0]][lcv[1]]:
+                                    if [lcv[0]+1,lcv[1]] not in matchList:
+                                        matchList.append([lcv[0]+1,lcv[1]])
+                                if left == rowList[lcv[0]][lcv[1]]:
+                                    if [lcv[0],lcv[1]-1] not in matchList:
+                                        matchList.append([lcv[0],lcv[1]-1])
+                                if right == rowList[lcv[0]][lcv[1]]:
+                                    if [lcv[0],lcv[1]+1] not in matchList:
+                                        matchList.append([lcv[0],lcv[1]+1])
+
+                        if len(matchList) == 4:
+                            delList.append([matchList[0],matchList[1],matchList[2],matchList[3]])
+                            
+                        for i in matchList:
+                            trashList.append([i[0],i[1]])
+                        if matchList != []:
+                            matchList = []
+                    #This prints the values around the cell that we care about for debugging
+                    #print(f"Above: {above}")
+                    #print(f"Below: {below}")
+                    #print(f"Left: {left}")
+                    #print(f"Right: {right}")
+                    #input()
+                else:
+                    trashList.append([row,cell])
+    if len(delList) >0:
+        delAnimation(delList,3)
+        for i in delList:
+            for j in i:
+                rowList[j[0]][j[1]] = "   "
+
+def delAnimation(givenList,width):
+    for i in givenList:
+        for j in i:
+            os.system('clear')
+            tempList = []
+            tempList.append(rowList[j[0]][j[1]])
+            if str(tempList)[3:10] == "x1b[41m":
+                rowList[j[0]][j[1]] = Back.LIGHTRED_EX + "   " + Back.RESET
+            elif str(tempList)[3:10] == "x1b[43m":
+                rowList[j[0]][j[1]] = Back.LIGHTYELLOW_EX + "   " + Back.RESET
+            if width == 3:
+                threeXThree()
+            elif width == 4:
+                fourXFour()
+            elif width == 5:
+                fiveXFive()
+            elif width == 6:
+                sixXSix()
+            time.sleep(0.35)
+        for j in i:
+            os.system('clear')
+            rowList[j[0]][j[1]] = " "+Back.BLACK+" "+Back.RESET+" "
+        if width == 3:
+            threeXThree()
+        elif width == 4:
+            fourXFour()
+        elif width == 5:
+            fiveXFive()
+        elif width == 6:
+            sixXSix()
+        time.sleep(0.35)
+        
+
+
 
 
 resetCells()
 levelOneSettings()
-
+#testSettings()
+while True:
+    os.system('clear')
+    
+    threeXThree()
+    act = input()
+    action(act,3)
+    checkCells(3)
+    
+    
 
--- test.py
+++ test.py
@@ -2,12 +2,27 @@ import os
 from colorama import Fore, Style, Back
 z=os.get_terminal_size()[0]
 
-x = Back.BLUE + "   " + Back.RESET
-y = len(Back.BLUE + Back.RESET)
-print(f"|{x}|".center(z+y))
+testList = ["poop"]
 
+for i in range(len(testList)):
+    if testList[i] == "poop":
+        testList.append("fart")
+
+x = "poop"
+testDict = {}
+testDict[x] = "fart"
+testDict["mr"] = "johnson"
+print(testDict)
+for i in testDict:
+    print(i)
+myList = [Back.LIGHTRED_EX + "   "+ Back.RESET]
+print(myList[0])
+print(myList)
+print(str(myList))
+print(str(myList)[3:10])
+print((Back.LIGHTYELLOW_EX+"|   |"+Back.RESET).center(z+10))
 print("|   |".center(z))
-print(len(Back.BLUE))
-print(len(Back.RED))
-print(len(Back.GREEN))
-print(len(Back.CYAN))
+print(len(Back.YELLOW+Back.RESET)==len(Back.LIGHTYELLOW_EX+Back.RESET))
+x = Back.YELLOW + "   " + Back.RESET
+print(len(x))
+print(len(" "+Back.BLACK+" "+Back.RESET+" "))
